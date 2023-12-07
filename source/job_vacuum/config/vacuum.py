d7e02dfe6b16b27c4a4e519fdcd71486795c33d1
Douglas jr <douglasjr1985@gmail.com>
2023-12-07 08:10:49 -0300

    enviando arquivos

--- source/job_vacuum/config/vacuum.py
+++ source/job_vacuum/config/vacuum.py
@@ -1,39 +1,51 @@
-from concurrent.futures import ProcessPoolExecutor
+import json
+from concurrent.futures import ThreadPoolExecutor
 from delta.tables import DeltaTable
 from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
 
+class VacuumJob:
+    def __init__(self, spark_session, config_file, max_threads=10):
+        self.spark = spark_session
+        self.config_file = config_file
+        self.max_threads = max_threads
+        self.config_data = self.load_config()
 
+    def load_config(self):
+        with open(self.config_file, 'r') as file:
+            return json.load(file)
 
-# Função para inicializar o Spark e executar o VACUUM
-def vacuum_table(database_name, table_name, retention_hours):
-    spark = SparkSession.builder.appName("VacuumJob").getOrCreate()
-    delta_table = DeltaTable.forName(spark, f"{database_name}.{table_name}")
-    delta_table.vacuum(retention_hours)
-    spark.stop()
+    def vacuum_table(self, database_name, table_name, retention_hours=24*7):
+        try:
+            delta_table = DeltaTable.forName(self.spark, f"{database_name}.{table_name}")
+            delta_table.vacuum(retention_hours)
+            print(f"Vacuum concluído em {database_name}.{table_name}")
+        except AnalysisException:
+            print(f"Tabela {database_name}.{table_name} não encontrada.")
 
+    def vacuum_wrapper(self, table_info):
+        self.vacuum_table(table_info['database_name'], table_info['table_name'])
 
-# Função para extrair informações e chamar vacuum_table
-def vacuum_wrapper(table_info):
-    database_name = table_info['database_name']
-    table_name = table_info['table_name']
-    retention_hours = 24 * 7  # Defina o período de retenção conforme necessário
-    print(f"Vacuuming {database_name}.{table_name}")
-    vacuum_table(database_name, table_name, retention_hours)
+    def get_tables_info(self):
+        tables_info = []
+        for database_name in self.config_data['database_name']:
+            db_tables = (
+                self.spark
+                .sql(f"SHOW TABLES FROM {database_name}")
+                .rdd
+                .map(lambda row: {'database_name': row['database'], 'table_name': row['tableName']})
+                .collect()
+            )
+            db_tables = [table for table in db_tables if table['table_name'] not in self.config_data['skip_tables']]
+            tables_info.extend(db_tables)
+        return tables_info
 
-# Usando ProcessPoolExecutor para executar vacuum_table em paralelo
-with ProcessPoolExecutor(max_workers=len(tables_info)) as executor:
-    futures = [executor.submit(vacuum_wrapper, table_info) for table_info in tables_info]
+    def run_parallel_vacuum(self):
+        tables_info = self.get_tables_info()
+        with ThreadPoolExecutor(max_workers=min(len(tables_info), self.max_threads)) as executor:
+            futures = [executor.submit(self.vacuum_wrapper, table_info) for table_info in tables_info]
+            for future in futures:
+                future.result()
 
-    while futures:
-        for future in futures:
-            if future.running():
-                print(f"Uma tarefa ainda está em execução {future.result()}")
-            elif future.done():
-                print(f"Tarefa concluída com resultado: {future.result()}")
-                futures.remove(future)
+        print(f"Vacuum realizado em {len(tables_info)} tabelas.")
 
-        time.sleep(0.5)  # Espera antes de verificar novamente
-
-    # # Aguardar a conclusão de todas as tarefas
-    # for future in futures:
-    #     future.result()  # Isso irá bloquear até que a tarefa correspondente seja concluída    
--- source/job_vacuum/run_job_vacuum.py
+++ source/job_vacuum/run_job_vacuum.py
@@ -1,64 +1,17 @@
 import json
 import time
 
-
 from delta.tables import DeltaTable
-from pyspark.sql.utils import AnalysisException
-from pyspark.sql import SparkSession
 from concurrent.futures import ThreadPoolExecutor
- 
+from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
 
-# Caminho para o arquivo JSON
-arquivo_json = 'config/param.json'
-
-
-with open(arquivo_json, 'r') as file:
-   data = json.load(file)
-
-# Lista para armazenar as informações das tabelas
-tables_info = []
-
-# Iterando sobre os bancos de dados
-for database_name in data['database_name']:
-    # Obtém as tabelas do banco de dados
-    db_tables = (
-        spark
-        .sql(f"SHOW TABLES FROM {database_name}")
-        .rdd
-        .map(lambda row: {'database_name': row['database'], 'table_name': row['tableName']})
-        .collect()
-    )
-
-    # Filtra as tabelas a serem puladas
-    db_tables = [table for table in db_tables if table['table_name'] not in data['skip_tables']]
-
-    # Adiciona as tabelas filtradas à lista geral
-    tables_info.extend(db_tables)
+from config.vacuum import VacuumJob
 
 
-# Agora, 'tables' contém as informações das tabelas de todos os bancos de dados, exceto aquelas que são para serem puladas
-print(f'quantidade de tabelas: {len(tables_info)}')
-
-# Função para inicializar o Spark e executar o VACUUM)
-def vacuum_table(database_name, table_name, retention_hours):
+if __name__ == "__main__":
     spark = SparkSession.builder.appName("VacuumJob").getOrCreate()
-    delta_table = DeltaTable.forName(spark, f"{database_name}.{table_name}")
-    delta_table.vacuum(retention_hours)
-    #spark.stop()
-
-
-# Função para extrair informações e chamar vacuum_table
-def vacuum_wrapper(table_info):
-    database_name = table_info['database_name']
-    table_name = table_info['table_name']
-    retention_hours = 24 * 7  # Defina o período de retenção conforme necessário
-    print(f"Vacuuming {database_name}.{table_name}")
-    vacuum_table(database_name, table_name, retention_hours)
-
-# Usando ThreadPoolExecutor para executar vacuum_table em paralelo
-with ThreadPoolExecutor(max_workers=len(tables_info)) as executor:
-    futures = [executor.submit(vacuum_wrapper, table_info) for table_info in tables_info]
-
-    # Aguardar a conclusão de todas as tarefas
-    for future in futures:
-        future.result()  # Isso irá bloquear até que a tarefa correspondente seja concluída
+    spark.conf.set('spark.databricks.delta.vacuum.parallelDelete.enabled', 'true')
+    
+    vacuum_job = VacuumJob(spark, "config/param.json")
+    vacuum_job.run_parallel_vacuum()
