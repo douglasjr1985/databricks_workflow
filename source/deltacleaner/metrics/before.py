6b6dece5e98e9d86bb5f56f9513eb67c7b44283a
Douglas <douglasjr1985@gmail.com>
2023-12-19 13:07:08 -0300

    send files

--- source/deltacleaner/metrics/before.py
+++ source/deltacleaner/metrics/before.py
@@ -64,30 +64,6 @@ class DeltaTableMetricsCollectorBefore:
             logging.warning(f"Database {database_name} does not exist.")
             return False
 
-    def check_need_for_optimize(self, database_name, table_name, threshold=128 * 1024 * 1024):
-        try:
-            delta_table = DeltaTable.forName(self.spark, f"{database_name}.{table_name}")
-            df_detail = delta_table.detail()
-            stats = df_detail.select("numFiles", "sizeInBytes").first()
-            num_files = stats["numFiles"]
-            total_size = stats["sizeInBytes"]
-
-            if num_files:
-                average_file_size = total_size / num_files
-                return average_file_size < threshold
-            else:
-                logging.info("No files found in the table.")
-                return False
-
-        except Exception as e:
-            logging.error(f"Error checking need for optimize in {table_name}: {e}")
-            return False   
-        
-    def check_need_for_vacuum(self, database_name, table_name, threshold=1000):
-        history_df = self.spark.sql(f"DESCRIBE HISTORY `{database_name}`.`{table_name}`")
-
-        return history_df.count() > threshold        
-
     def get_tables_modified_today(self):
         """
         Retrieves tables that were modified today in the specified databases.
@@ -104,13 +80,12 @@ class DeltaTableMetricsCollectorBefore:
                     tables_df = self.spark.sql(f"SHOW TABLES IN {database_name}")
                     for row in tables_df.collect():
                         table_name = row.tableName
-                        if self.check_need_for_optimize(database_name, table_name) or self.check_need_for_vacuum(database_name, table_name):
-                            history_query = f"DESCRIBE HISTORY {database_name}.{table_name}"
-                            history_df = self.spark.sql(history_query)
-                            last_modified_row = history_df.orderBy(F.desc("timestamp")).first()
+                        history_query = f"DESCRIBE HISTORY {database_name}.{table_name}"
+                        history_df = self.spark.sql(history_query)
+                        last_modified_row = history_df.orderBy(F.desc("timestamp")).first()
 
-                            if last_modified_row and last_modified_row.timestamp.date() == current_date:
-                                modified_tables.append((database_name, table_name))
+                        if last_modified_row and last_modified_row.timestamp.date() == current_date:
+                           modified_tables.append((database_name, table_name))
                 except Exception as e:
                     logging.error(f"Error checking modification date for tables in database {database_name}: {e}")
 
